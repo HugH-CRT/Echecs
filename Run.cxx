2ae0da3753ae84ac9792347efaaab2e7f100af88
Yoan Laurain <80961060+verozgames@users.noreply.github.com>
2021-10-24 15:08:09 +0200

    make clean

---
+++ Run.cxx
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <assert.h>
+#include "Piece.h"
+#include "Joueur.h"
+#include "Echiquier.h"
+using namespace std;
+
+bool
+compare_position( Piece &p, Piece &q )
+{
+    q = p;gi
+    q.affiche();
+    return ( p.x() == q.x() ) && ( p.y() == q.y() );
+}
+
+int
+main( int argc, char **argv )
+{
+    Echiquier e;
+    e.affiche();
+
+    JoueurBlanc jb;
+    JoueurNoir  jn;
+
+    jb.placerPieces( e ) ;
+    jn.placerPieces( e ) ;
+
+    e.affiche();
+
+}
